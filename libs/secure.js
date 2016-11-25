f660f4f1a360c920bcf5cc2b859f01f6ab6edebc
endlessdev <ysw0094@gmail.com>
2016-11-25 22:25:36 +0900

    Add isVerifyToken function to secure.js file

--- libs/secure.js
+++ libs/secure.js
@@ -1,11 +1,29 @@
 /**
  * Created by jade on 06/11/2016.
  */
-let jwt = require('jsonwebtoken');
+let jwt = require('jsonwebtoken'),
+    User = require('../models/users');
 
 exports.generateToken = function (userId) {
     return jwt.sign({'userId': userId}, process.env.JWT_SECRET, {
         algorithm: 'HS256',
         expiresIn: '1440m'
-    })
+    });
 };
+
+exports.isVerifyToken = function (userToken, handleQuery) {
+    User.findOne({'userToken': userToken}, function (err, user){
+        handleQuery(err, user);
+        if (!err) {
+            if (user) {
+                return user;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    });
+}
+
+//TODO generateHash function for users password encrypt
